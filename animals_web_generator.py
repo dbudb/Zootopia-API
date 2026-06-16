8848388ff335395517d1fb8c56b9922a74e28e30
Dennis Budek <dennis.budek@protonmail.com>
2026-06-16 17:28:24 +0200

    dict to string

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -8,6 +8,8 @@ def load_data(file_path):
 
 animals_data = load_data('animals_data.json')
 
+selected_animal_data = ""
+
 for animal in animals_data:
     name = animal.get("name")
     diet = animal.get("characteristics").get("diet")
@@ -15,11 +17,21 @@ for animal in animals_data:
     main_location = locations[0]
     animal_type = animal.get("characteristics").get("type")
 
-    print(f"Name: {name}")
+    print(f"Name: {name}\n")
+    selected_animal_data += f"Name: {name}\n"
     if diet and diet != "None":
+        selected_animal_data += f"Diet: {diet}\n"
         print(f"Diet: {diet}")
     if main_location and main_location != "None":
+        selected_animal_data += f"Location: {main_location}\n"
         print(f"Location: {main_location}")
     if animal_type and animal_type != "None":
+        selected_animal_data += f"Type: {animal_type}\n"
         print(f"Type: {animal_type}")
+    selected_animal_data += "\n"
     print()
+
+
+print(50*"\n")
+
+print(selected_animal_data)
