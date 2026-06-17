2906a31f656554e227e69dd1c47ba704feadd5d5
Dennis Budek <dennis.budek@protonmail.com>
2026-06-17 16:39:51 +0200

    cleaning part II - return of the animal data

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -2,49 +2,52 @@ import json
 from typing import Any
 
 
-def load_data(file_path: str) -> Any:
+def load_data(file_path: str) -> list[dict[str, Any]]:
     """Loads a JSON file"""
     with open(file_path, "r", encoding="utf-8") as handle:
         return json.load(handle)
 
 
 def load_template(file_path: str) -> str:
-    "loads a textfile"
+    """loads a textfile"""
     with open(file_path, "r", encoding="utf-8") as file:
         return file.read()
 
 
-def save_html(file_path: str, content: str) -> None:
+def save_file(file_path: str, content: str) -> None:
+    """saves a text file"""
     with open(file_path, "w", encoding="utf-8") as file:
         file.write(content)
 
 
-def serialize_output(animal):
+def serialize_output(animals):
+    selected_animal_data = ""
+    for animal in animals:
+        name = animal.get("name")
+        diet = animal.get("characteristics").get("diet")
+        locations = animal.get("locations")
+        main_location = locations[0]
+        animal_type = animal.get("characteristics").get("type")
 
-animals_data = load_data("animals_data.json")
-selected_animal_data = ""
-html_template = load_template("animals_template.html")
+        selected_animal_data += "<li class='cards__item'>"
+        selected_animal_data += f"<div class='card__title'>{name}</div>"
+        selected_animal_data += f"<p class='card__text'>"
 
-for animal in animals_data:
+        if diet and diet != "None":
+            selected_animal_data += f"<strong>Diet:</strong> {diet}<br/>"
+        if main_location and main_location != "None":
+            selected_animal_data += f"<strong>Location:</strong> {main_location}<br/>"
+        if animal_type and animal_type != "None":
+            selected_animal_data += f"<strong>Type:</strong> {animal_type}<br/>"
+        selected_animal_data += "</p></li>"
+    return selected_animal_data
 
-    name = animal.get("name")
-    diet = animal.get("characteristics").get("diet")
-    locations = animal.get("locations")
-    main_location = locations[0]
-    animal_type = animal.get("characteristics").get("type")
+def main():
+    animals_data = load_data("animals_data.json")
+    html_template = load_template("animals_template.html")
+    animals_html = serialize_output(animals_data)
+    html_filled = html_template.replace("__REPLACE_ANIMALS_INFO__", animals_html)
+    save_file("animals.html", html_filled)
 
-    selected_animal_data += "<li class='cards__item'>"
-    selected_animal_data += f"<div class='card__title'>{name}</div>"
-    selected_animal_data += f"<p class='card__text'>"
-
-    if diet and diet != "None":
-        selected_animal_data += f"<strong>Diet:</strong> {diet}<br/>"
-    if main_location and main_location != "None":
-        selected_animal_data += f"<strong>Location:</strong> {main_location}<br/>"
-    if animal_type and animal_type != "None":
-        selected_animal_data += f"<strong>Type:</strong> {animal_type}<br/>"
-
-    selected_animal_data += "</p></li>"
-
-html_filled = html_template.replace("__REPLACE_ANIMALS_INFO__", selected_animal_data)
-save_html("animals.html", html_filled)
+if __name__ == "__main__":
+    main()
