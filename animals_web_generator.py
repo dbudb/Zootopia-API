59a15bb01d1741b1c83f65d678614fc876c21558
Dennis Budek <dennis.budek@protonmail.com>
2026-06-17 16:10:50 +0200

    cleaning part I

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -1,44 +1,45 @@
 import json
 
-def load_data(file_path):
+def load_data(file_path: str) -> json:
     """Loads a JSON file"""
     with open(file_path, "r", encoding="utf-8") as handle:
         return json.load(handle)
 
 
-animals_data = load_data('animals_data.json')
+def load_template(file_path: str) -> str:
+    "loads a textfile"
+    with open(file_path, "r", encoding="utf-8") as file:
+        return file.read()
+
+def save_html(file_path: str, content: str) -> None:
+    with open("animals.html", "w", encoding="utf-8") as file:
+        file.write(html_filled)
+
+
+animals_data = load_data("animals_data.json")
 selected_animal_data = ""
+html_template = load_template("animals_template.html")
+
 for animal in animals_data:
+
     name = animal.get("name")
     diet = animal.get("characteristics").get("diet")
     locations = animal.get("locations")
     main_location = locations[0]
     animal_type = animal.get("characteristics").get("type")
 
-    print(f"Name: {name}")
-    print(f"Diet: {diet}")
-    print(f"Location: {main_location}")
-    print(f"Type: {animal_type}")
-    print()
     selected_animal_data += "<li class='cards__item'>"
     selected_animal_data += f"<div class='card__title'>{name}</div>"
     selected_animal_data += f"<p class='card__text'>"
+
     if diet and diet != "None":
         selected_animal_data += f"<strong>Diet:</strong> {diet}<br/>"
     if main_location and main_location != "None":
         selected_animal_data += f"<strong>Location:</strong> {main_location}<br/>"
     if animal_type and animal_type != "None":
         selected_animal_data += f"<strong>Type:</strong> {animal_type}<br/>"
+
     selected_animal_data += "</p></li>"
 
-print(selected_animal_data)
-
-with open("animals_template.html", "r", encoding="utf-8") as file:
-    html_template = file.read()
-
 html_filled = html_template.replace("__REPLACE_ANIMALS_INFO__", selected_animal_data)
-
-with open("animals.html", "w", encoding="utf-8") as file:
-    file.write(html_filled)
-
-print(html_filled)
+save_html("animals.html", html_filled)
